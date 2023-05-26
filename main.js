4abad90f9453235e67cc7da10788c44945a36524
Jean0405 <jeanpierrea0405@gmail.com>
2023-05-25 22:24:05 -0500

    feat: :sparkles: Se crearon las clases
    Se crea la clase padre ANIMAL y su hija la clase PERRO, las cuales reciben los datos del formulario para ser instanciadas

--- main.js
+++ main.js
@@ -72,4 +72,59 @@ FORM.addEventListener("submit", (e) => {
     persona1.getEdad
   )}</p>
   `;
+  document.querySelector(".saludo-container").classList.add("show");
+  FORM.reset();
+});
+
+// PUNTO 2 CLASE ANIMALES
+class Animal {
+  constructor({ nombre, edad }) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+  get getNombre() {
+    return this.nombre;
+  }
+  get getEdad() {
+    return this.edad;
+  }
+
+  hacerSonido() {
+    return `El animal hace sonido`;
+  }
+}
+
+class Perro extends Animal {
+  constructor({ nombre, edad, raza }) {
+    super({ nombre, edad });
+    this.raza = raza;
+  }
+  get getRaza() {
+    return this.raza;
+  }
+
+  moverCola() {
+    return `El perro está moviendo la cola`;
+  }
+}
+
+const mensajeP2 = document.querySelector("#mensaje2");
+const FORM_ANIMAL = document.querySelector("#form-animal");
+FORM_ANIMAL.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let data = Object.fromEntries(new FormData(e.target));
+
+  const animal1 = new Animal(data);
+  const perro1 = new Perro(data);
+
+  mensajeP2.innerHTML = `
+  <p class="text-light fst-italic fw-bold">${animal1.getNombre} (${
+    perro1.getRaza
+  })</p>
+  <p class="text-light fst-italic">${animal1.getEdad} años</p>
+  <p class="text-light fst-italic">${animal1.hacerSonido()}</p>
+  <p class="text-light fst-italic">${perro1.moverCola()}</p>
+  `;
+  document.querySelector(".saludo-containerAnimal").classList.add("show");
+  FORM_ANIMAL.reset();
 });
